5b2cfb5193ada1fbcdc218ab94e4e55cd8d18f1b
cade <cadeworm+git2@gmail.com>
2022-03-10 15:13:03 -0600

    v1.1: Added comments and handled some errors better.

--- GitGet.js
+++ GitGet.js
@@ -3,44 +3,70 @@
     Copyright (C) 2022  Cade Wormington
 
     GitGet sends API queries to GitHub to obtain information about a given user's repositories. Make sure to modify
-    the settings in var declarations (jsonPath, user) to your liking if you are not importing this file. To use, just call exports.main()
-    in this file or simply main() if you have imported it. 
+    the settings in var declarations (jsonPath, user) to your liking if you are not importing this file. To use, just call 
+    exports.main() in this file or simply main() if you have imported it. 
     
-    exports.main(mode, tokPath) - mode: 'write' or 'return'. 'write' will write the received JSON to the file specified in 
-                                  the jsonPath constant below. 'return' will cause the main() function to return the JSON as 
-                                  a string.
+    exports.main(user, auth, mode, writeDir) - user: The username of the user who we want to see the repositories of.
 
-                                  tokPath: The filepath which contains nothing except text of a user's GitHub API token. This
-                                  is used to authenticate the API requests.
+                                               auth: The filepath of the GitHub API authorization token which we want to 
+                                               send the request with. Supplying this increases your API rate limit.
+                                                
+                                               mode: 'write' or 'return'. 'write' will write the received JSON to the file 
+                                               specified in the jsonPath constant below. 'return' will cause the main() 
+                                               function to return the JSON as a string.
+
+                                               writeDir: The filepath which defines the location and name of the output 
+                                               file. The name of the file should be included in the path.
 */
 
 
-/**
- *  ===== IMPORTS =====
+/*
+    ===== IMPORTS =====
  */
 const { Octokit } = require('@octokit/rest');
 const fsProm = require('fs').promises;
 
-/**
- *  ===== VAR DECLARATIONS =====
+/*
+    ===== VAR DECLARATIONS =====
  */
 let username;
 let token;
 let repoList;
+
+/*
+    ===== FUNCTIONS =====
+ */
+
 /**
- *  ===== FUNCTIONS =====
+ * readAuth() is a function which takes a filepath and opens the file
+ * at that location. It then reads the text in that file as a GitHub
+ * API token and stores it.
+ * 
+ * @param {string} filePath - the path to the file containing the token 
  */
 const readAuth = async (filePath) => {
     await fsProm.readFile(filePath, 'utf8').then((fileData) => {
         token = fileData;
     }).catch((err) => {
         console.error(err);
+        token = null;
     });
 };
 
+/**
+ * pullData() filters through the data returned by the API call. A lot of info is
+ * returned with this call, and we only want the data that we need.
+ * 
+ * @param {array: object} rawData - The array of objects which contain information about
+ *                                  a user's GitHub repositories.
+ * 
+ * @returns - null if rawData is given an invalid array or an empty array.
+ *            Otherwise, the array of repository objects with the targeted
+ *            data is returned.
+ */
 const pullData = (rawData) => {
-    if (!rawData) {
-        return;
+    if (!rawData || rawData.length === 0) {
+        return null;
     }
     let accum = [];
     // Pull only the important data. (name, updated_at, owner.login, description, html_url)
@@ -57,6 +83,14 @@ const pullData = (rawData) => {
     return accum;
 };
 
+/**
+ * makeReq() is the function which constructs and sends the request for repository information
+ * to the GitHub API. It takes the result of the API call and passes it as a parameter to 
+ * pullData() which cleans and filters the data. It then returns the clean data.
+ * 
+ * @returns - null if a HTTP response with a status other than '200 OK' is received.
+ *            Otherwise, the clean data is returned.
+ */
 const makeReq = async () => {
     const kit = new Octokit({
         auth: token,
@@ -85,34 +119,63 @@ const makeReq = async () => {
     return null;
 };
 
-
+/**
+ * writeJson() is the function which writes the JSON data to a file if the user 
+ * selects the 'write' mode.
+ * 
+ * @param {*} repoArr 
+ * @param {*} writePath 
+ */
 const writeJson = async (repoArr, writePath) => {
     await fsProm.writeFile(writePath, JSON.stringify(repoArr));
 };
 
-/**
- *  ===== SCRIPT =====
+/*
+    ===== SCRIPT =====
  */
 
+/**
+ * main() is the primary function of the program. Call this to use GitGet.
+ * 
+ * @param {*} user      - The username of the user who we want to see the repositories of.
+ * 
+ * @param {*} auth      - The filepath of the GitHub API authorization token which we want to
+                        send the request with. Supplying this increases your API rate limit.      
+
+ * @param {*} mode      - mode: 'write' or 'return'. 'write' will write the received JSON to 
+                        the file specified in the jsonPath constant below. 'return' will 
+                        cause the main() function to return the JSON as a string.
+
+ * @param {*} writeDir  - The filepath which defines the location and name of the output 
+                         file. The name of the file should be included in the path.
+ * 
+ * @returns - the JSON string of repository objects if in 'return' mode.
+ *          - null if an invalid mode is given.
+ *          - nothing otherwise.
+ */
 exports.main = async (user, auth, mode, writeDir) => {
     username = user;
     await readAuth(auth);
     
     if (mode === "write") {
         if (!writeDir) {
-            console.error("GitGet: write mode without output path.");
+            console.error("GitGet: write mode without output filepath.");
             return;
         }
         let data = await makeReq();
         if (data) {
             await writeJson(data, writeDir);
+            return;
         }
     } else if (mode === "return") {
-        return JSON.stringify(await makeReq());
+        let data = await makeReq();
+        if (data) {
+            return JSON.stringify(data);
+        }
     } else {
         console.error("GitGet: invalid mode. " + mode);
         return null;
     }
 };
 
-// exports.main("write");
+// exports.main("<username>", "<authTokenPath>", "write", "./output.txt");
--- README.md
+++ README.md
@@ -1,11 +1,24 @@
 # GitGet
-A simple JavaScript webapp which queries the GitHub API to obtain information about a given account's public repositories.
 
+## Summary
+A simple JavaScript webapp which queries the GitHub API to obtain information about a given account's public repositories. 
+It receives the information from the API in JSON and summarizes it into a more condensed form. The user is given the option to
+have the program return its data or write it to a file.
 
+This program can be run as a script or imported and run in another program. Just call exports.main() inside the file or import it
+and call the main() function.
 
+**Call within GitGet.js:**
+`exports.main(<target username>, <auth token path>, <mode: 'return' or 'write'>, <output path>);`
+*Note: output path is only needed if in write mode.*
 
+**Call in another program:**
+`const GitGet = require(<path to GitGet.js>);
+exports.main(<target username>, <auth token path>, <mode: 'return' or 'write'>, <output path>);`
 
+---
 
+## License
 GitGet - A basic JavaScript program used to query the GitHub API using the NPM @octokit/rest module for Node.js.
 Copyright (C) 2022  Cade Wormington
 
